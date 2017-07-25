438c088aa9f7f21a2c20159b83906100c1b15e0e
Claire Johnson <c.eliseee@gmail.com>
2017-07-25 15:19:23 -0700

    Added base checks for JSON grammar types

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,45 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  // let el = json[0];
+
+  let recurse = function(el) {
+    //String check
+    if(el[0] === '"') {
+      let final = '';
+      return final + json.slice(1, json.length - 1);
+    }
+
+    //Number check
+    if(/^[0-9]+$/.test(el)) {
+      return parseInt(el, 10);
+    }
+
+    //Other check
+    if(el === 'null') return null;
+    if(el === 'true') return true;
+    if(el === 'false') return false;
+
+    //Object function
+    if(el[0] === '{') {
+      let obj = {};
+      return parseObj(el);
+    }
+
+    //Array function
+    if(el[0] === '[') {
+      let arr = [];
+      return parseArr(el);
+    }
+
+  }
+
+
+
+
 };
+
+
+//Create recursive function for every type of JSON grammar (object, string, array, etc)
+//Create nextChar function that gets the next character to be parsed and ignores unecessary whitespace.
+//Go into recursive function for overall object and then call other functions as needed for object elements.
