a0bbdd2a248bbe24023223bad9c4ab8f489970db
Claire Johnson <c.eliseee@gmail.com>
2017-07-24 20:26:54 -0700

    Function passes all tests. Uses inner recursion function to find all nodes with matching class names.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,33 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+var getElementsByClassName = function(className) {
+    let final = [];
+
+    if (document.body.className === className) final.push(document.body);
+    let nodes = document.body.childNodes;
+    let recurse = (element) => {
+        //console.log(element);
+        if (element === undefined) return;
+        if (element.classList === undefined) {
+
+        } else if (element.classList.contains(className)) {
+            final.push(element);
+        }
+        if (element.childNodes.length > 0) {
+            for (let i = 0; i < element.childNodes.length; i++) {
+                recurse(element.childNodes[i]);
+            }
+        }
+
+    };
+
+    for (let i = 0; i < nodes.length; i++) {
+        //console.log('Going into: ' + nodes[i]);
+        recurse(nodes[i]);
+    }
+
+    return final;
+
 };
