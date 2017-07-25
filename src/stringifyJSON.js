007026a297b22eba1100ca28a785c3f6dd083aa7
Claire Johnson <c.eliseee@gmail.com>
2017-07-24 19:04:40 -0700

    Script passes all tests. Cleaned up code and fixed bugs in object handler and null/undefined handlers.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,29 +4,32 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  let final = '';
   let arrElements = [];
-  let objKeys = [];
-  let objElem = [];
   let arrObj = [];
-  if(typeof obj === 'number' || typeof obj ==='boolean' || typeof obj === null) {
-    return final += obj;
+
+  if(typeof obj === 'number' || typeof obj ==='boolean' || obj === null) {
+    return '' + obj;
   }
-  if(typeof obj = 'string') return '"' + obj + '"';
+
+  if(typeof obj === 'string') return '"' + obj + '"';
+
+  if(typeof obj === 'undefined' || typeof obj === 'function') return null;
+
   if(Array.isArray(obj)) {
-    // final += '[';
-    // return final + obj.reduce((acc, cur) => {
     for(let i = 0; i < obj.length; i++) {
       arrElements.push(stringifyJSON(obj[i]));
     }
+
     return '[' + arrElements.join(',') + ']';
     }
-  if (typeof obj === 'object') {
-    // let keys = Object.keys(obj);
-    for(keys in obj) {
-      arrObj.push(`${keys}: ${stringifyJSON(obj[keys])}`)
-    return '{' + arrObj.join(',') + '}'
-    }
-  }
 
+  if(typeof obj === 'object') {
+    for(let keys in obj) {
+      if(stringifyJSON(obj[keys])) {
+        arrObj.push(`"${keys}":${stringifyJSON(obj[keys])}`)
+      }
+    }
+
+    return '{' + arrObj.join(',') + '}';
+  }
 };
