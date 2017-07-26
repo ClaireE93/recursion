114021adb8aef3a15aa9d0e776401ebf99576be3
Claire Johnson <c.eliseee@gmail.com>
2017-07-26 11:07:14 -0700

    More code cleanup

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -1,21 +1,20 @@
 // this is what you would do if you were one to do things the easy way:
 // var parseJSON = JSON.parse;
-
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
 
   let recurse = function(el) {
     //First string check
-    if(el === 'null') return null;
-    if(el === 'true') return true;
-    if(el === 'false') return false;
+    if (el === 'null') return null;
+    if (el === 'true') return true;
+    if (el === 'false') return false;
 
     //All other string check
-    if(el[0] === '\"') {
+    if (el[0] === '\"') {
       let final = '';
-      for(let i = 1; i < el.length - 1; i++) {
-        if(el[i] === '\\') {
-          final += el[i+1];
+      for (let i = 1; i < el.length - 1; i++) {
+        if (el[i] === '\\') {
+          final += el[i + 1];
           i++;
         } else {
           final += el[i];
@@ -25,17 +24,17 @@ var parseJSON = function(json) {
     }
 
     //Number check
-    if(isNum(el)) {
+    if (isNum(el)) {
       return parseFloat(el);
     }
 
     //Object function
-    if(el[0] === '{') {
+    if (el[0] === '{') {
       return parseObj(el);
     }
 
     //Array function
-    if(el[0] === '[') {
+    if (el[0] === '[') {
       return parseArr(el);
     }
 
@@ -44,27 +43,29 @@ var parseJSON = function(json) {
   //Check for negatives, floats, and ints. Check that element is primitive value,
   //not a value in an array/object.
   let isNum = el => {
-      let isArr = el[0] === '[' || el[0] === '{';
-      let test = /^[0-9]+$/.test(el[0]) || /^[0-9]+$/.test(el[1]);
-      return test && !isArr;
-    };
+    let isArr = el[0] === '[' || el[0] === '{';
+    let test = /^[0-9]+$/.test(el[0]) || /^[0-9]+$/.test(el[1]);
+
+    return test && !isArr;
+  };
 
   let parseObj = el => {
     let obj = {};
-    if(el[1] === '}') return obj;
+    if (el[1] === '}') return obj;
     let keyValuePairs = getKeyValuePairsArr(el);
     keyValuePairs.forEach(curPair => {
       let curKey = getKeyString(curPair).trim();
-      curKey = curKey.slice(1, curKey.length - 1);          //Slice off unecessary quotation marks.
+      curKey = curKey.slice(1, curKey.length - 1); //Slice off unecessary quotation marks.
       let curVal = getValueString(curPair);
       obj[curKey] = recurse(curVal.trim());
     });
+    
     return obj;
   };
 
   let parseArr = el => {
     let arr = [];
-    if(el[1] === ']') return arr;
+    if (el[1] === ']') return arr;
     let arrEl = getKeyValuePairsArr(el);
     arrEl.forEach(cur => {
       arr.push(recurse(cur.trim()));
@@ -77,7 +78,7 @@ var parseJSON = function(json) {
   let getKeyString = el => {
     let str = '';
     let i = 0;
-    while(el[i] != ':') {
+    while (el[i] != ':') {
       str += el[i];
       i++;
     }
@@ -88,7 +89,7 @@ var parseJSON = function(json) {
   let getValueString = el => {
     let str = '';
     let i = 0;
-    while(el[i] != ':') {
+    while (el[i] != ':') {
       i++;
     }
     i++;
@@ -104,7 +105,7 @@ var parseJSON = function(json) {
     let isInString = false;
     let str = '';
     let depth = 0;
-    for(let i = 1; i < el.length; i++) {
+    for (let i = 1; i < el.length; i++) {
 
       //el.length check will fail if obj has extra whitespace. This catches the end of the object
       //and cuts off the extra bracket/curly brace that got added when el.length check failed.
@@ -115,7 +116,7 @@ var parseJSON = function(json) {
       }
 
       if (isInString) {
-        if (el[i] === '\"' && el[i-1] !== '\\') {
+        if (el[i] === '\"' && el[i - 1] !== '\\') {
           isInString = false;
         }
         str += el[i];
@@ -124,7 +125,7 @@ var parseJSON = function(json) {
 
       if (el[i] === '[' || el[i] === '{') {
         depth++;
-      } else if ( (el[i] === ']' || el[i] === '}') && (i === el.length - 1) ) { //Reached end of string
+      } else if ((el[i] === ']' || el[i] === '}') && (i === el.length - 1)) { //Reached end of string
         arr.push(str);
         str = '';
         break;
@@ -145,7 +146,7 @@ var parseJSON = function(json) {
     }
 
     //Throw error if JSON syntax is wrong
-    if(depth === 0 && str.length > 0) {
+    if (depth === 0 && str.length > 0) {
       throw new SyntaxError();
     }
 
